43ac9e044d99d1c62b6ac4ac80cc94e61486f38e
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-06-19 21:35:27 +0300

    add dropdown menu to navbar for user account options

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,14 @@ import Link from "next/link";
 
 import logo from "@/assets/images/logo.png";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export function Navbar() {
   return (
@@ -10,10 +18,25 @@ export function Navbar() {
       <Link href="/">
         <Image src={logo} alt="Logo" width={40} height={40} />
       </Link>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback className="text-black">AD</AvatarFallback>
-      </Avatar>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-black">AD</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   );
 }
