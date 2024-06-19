9f7bc05fbb4582e03efe837b8970a385a76a728a
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-06-19 21:11:33 +0300

    add logo to navbar and implement avatar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "@/assets/images/logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
 export function Navbar() {
   return (
     <nav className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
-      Navbar
+      <Link href="/">
+        <Image src={logo} alt="Logo" width={40} height={40} />
+      </Link>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback className="text-black">AD</AvatarFallback>
+      </Avatar>
     </nav>
   );
 }
