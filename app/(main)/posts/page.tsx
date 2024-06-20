9bc18d5a0138132272a0d3e8dc7a7230db91a652
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-06-20 16:00:29 +0300

    add BackButton and PostsPagination components

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,9 +1,13 @@
+import { BackButton } from "@/components/back-button";
+import { PostsPagination } from "@/components/posts/posts-pagination";
 import { PostsTable } from "@/components/posts/posts-table";
 
 export default function PostsPage() {
   return (
     <>
+      <BackButton text="Back to Dashboard" link="/" />
       <PostsTable />
+      <PostsPagination />
     </>
   );
 }
