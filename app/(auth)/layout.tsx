d4204daa5dfb033461d8c3cda581cb321d8c2c6a
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-06-22 23:33:40 +0300

    add theme toggler component to AuthLayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggler } from "@/components/theme-toggler";
 import { ReactNode } from "react";
 
 type Props = {
@@ -7,6 +8,9 @@ type Props = {
 export default function AuthLayout({ children }: Props) {
   return (
     <div className="h-[100vh] flex items-center justify-center relative">
+      <div className="absolute top-5 right-0 text-white">
+        <ThemeToggler />
+      </div>
       {children}
     </div>
   );
