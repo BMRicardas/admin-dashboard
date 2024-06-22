2b29f105f9f0fb00ee0d388284da7f9f8622edac
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-06-22 22:52:21 +0300

    add AuthTabs component

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,3 +1,9 @@
+import { AuthTabs } from "@/components/auth/auth-tabs";
+
 export default function AuthPage() {
-  return <div>AuthPage</div>;
+  return (
+    <>
+      <AuthTabs />
+    </>
+  );
 }
